abc9a84c0742e150ea6dd8eec1de5c0b050e9a85
warhammercasey <warhammercasey@gmail.com>
2018-07-02 01:00:31 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+  message.channel.send("Fuck you " + message.member.nickname)
+    .then(message => console.log(`Sent message: ${message.content}`))
+		.catch(console.error);
+});
+
+client.login(process.env.BOT_TOKEN);
